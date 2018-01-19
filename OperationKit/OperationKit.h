2d839f1542a61292818ffcacd997c3eb8f19f083
Florian Schliep <florian@appiculous.com>
2018-01-19 20:29:20 +0100

    Initial Commit

---
+++ OperationKit/OperationKit.h
@@ -0,0 +1,19 @@
+//
+//  OperationKit.h
+//  OperationKit
+//
+//  Created by Florian Schliep on 19.01.18.
+//  Copyright © 2018 Florian Schliep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OperationKit.
+FOUNDATION_EXPORT double OperationKitVersionNumber;
+
+//! Project version string for OperationKit.
+FOUNDATION_EXPORT const unsigned char OperationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OperationKit/PublicHeader.h>
+
+
